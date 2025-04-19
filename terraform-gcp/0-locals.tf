fab0b3d0d4140ea47537c16c819dd87872a98e17
Giovanni <giovy1232@gmail.com>
2025-04-19 11:49:07 +0200

    Update terraform 0-locals.tf file

--- terraform-gcp/0-locals.tf
+++ terraform-gcp/0-locals.tf
@@ -1,9 +1,13 @@
+variable "project_id" {
+	description = "ID del progetto GCP"
+	type        = string
+  
+}
+
 locals {
-	project_id = "my-project-1531942571796" # id del progetto
+	project_id = var.project_id # id del progetto
 	region     = "us-central1" # regione scelta
 	apis = [ # api utilizzate
-	"compute.googleapis.com", # per VPC e subnet
-	"container.googleapis.com", # per gestire k8s
 	"storage-component.googleapis.com", # per il bucket
 	"artifactregistry.googleapis.com", # per il registry
 	"iamcredentials.googleapis.com" # per il service account
