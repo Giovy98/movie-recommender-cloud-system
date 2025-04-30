f9fbd8f426e40cfa819a7ff45d375088ac54bf7f
Giovanni <giovy1232@gmail.com>
2025-04-30 16:56:20 +0200

    Fix

--- terraform-gcp/locals.tf
+++ terraform-gcp/locals.tf
@@ -4,7 +4,5 @@ locals {
   apis = [                              # api utilizzate
     "storage-component.googleapis.com", # per il bucket
     "artifactregistry.googleapis.com",  # per il registry
-    "compute.googleapis.com", # per VPC e subnet
-    "container.googleapis.com", # per gestire k8s  
   ]
 }
