63d1b2335cb184d32e35de3ee3fab2c6d94ec140
Giovanni <giovy1232@gmail.com>
2025-05-01 15:34:29 +0200

    Rollback

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -33,28 +33,83 @@ resource "google_artifact_registry_repository" "docker_repo" {
 }
 
 resource "google_compute_network" "vpc" {
-  project                 = var.project_id
-  name                    = "gke-vpc"
-  auto_create_subnetworks = true # Chiave per la semplicità!
-  routing_mode            = "REGIONAL" # O "GLOBAL", per auto-mode di solito è REGIONAL
+    name                    = "test-vpc"
+    routing_mode            = "REGIONAL"
+    auto_create_subnetworks = false
+    delete_default_routes_on_create = true
+   
+   depends_on = [google_project_service.api]
+}
+
+resource "google_compute_subnetwork" "public" { # subnet pubblica
+  name                     = "public"
+  ip_cidr_range            = "10.0.0.0/19" #Riserva un range di IP privati.
+  region                   = local.region
+  network                  = google_compute_network.vpc.id
+  private_ip_google_access = true # Le VM possono accedere ai servizi Google 
+  stack_type               = "IPV4_ONLY"
+}
+
+# subnet privata
+# Le VM non possono accedere a internet, ma possono accedere ai servizi Google
+# Le VM possono accedere a internet tramite un NAT gateway
+resource "google_compute_subnetwork" "private" {
+  name                     = "private"
+  ip_cidr_range            = "10.0.32.0/19"
+  region                   = local.region
+  network                  = google_compute_network.vpc.id
+  private_ip_google_access = true
+  stack_type               = "IPV4_ONLY"
+
+  secondary_ip_range{
+    range_name = "k8s-pods"
+    ip_cidr_range = "172.16.0.0/14" # Range di IP privati per i pod
+  }
+  secondary_ip_range {
+    range_name = "k8s-services"
+    ip_cidr_range = "172.20.0.0/18" # Range di IP privati per i servizi
+  }
+}
+
+# Opzionale 
+resource "google_compute_address" "nat" {
+  name         = "nat-terraform"
+  address_type = "EXTERNAL"
+  network_tier = "STANDARD"
 
   depends_on = [google_project_service.api]
 }
 
-resource "google_container_cluster" "gke_cluster" {
-  project  = var.project_id
-  name     = "gke-cluster"
-  location = local.region
-
+resource "google_compute_router" "router" {
+  name    = "router-terraform"
+  region  = local.region
   network = google_compute_network.vpc.id
-
-  remove_default_node_pool = false
-  initial_node_count       = 3 # Inizia con un singolo nodo nel pool di default
-
-  ip_allocation_policy {}
-
-  depends_on = [
-    google_compute_network.vpc,
-    google_project_service.api
-  ]
 }
+
+resource "google_compute_router_nat" "nat" {
+  name   = "nat-router"
+  region = local.region
+  router = google_compute_router.router.name
+
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ips                            = [google_compute_address.nat.self_link]
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private.self_link
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
+resource "google_compute_firewall" "allow_iap_ssh" {
+  name    = "allow-iap-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8000", "8080","8501", "22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
+
