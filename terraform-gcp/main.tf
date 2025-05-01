51bb5095ad8f3196af91160728ce5a3cac7d9095
Giovanni <giovy1232@gmail.com>
2025-05-01 21:53:03 +0200

    add vpc and gke from terraform

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -135,3 +135,55 @@ resource "google_compute_firewall" "allow_all_egress" {
     protocol = "all"
   }
 }
+
+resource "google_container_cluster" "gke_cluster" {
+  name     = "gke-cluster"
+  location = "us-west1-a"
+
+  remove_default_node_pool = false
+  initial_node_count       = 3  # Richiesto anche se non usato
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.private.name
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pods"
+    services_secondary_range_name = "k8s-services"
+  }
+
+  private_cluster_config {
+    enable_private_nodes = false
+  }
+
+  depends_on = [google_project_service.api]
+}
+
+resource "google_container_node_pool" "default_pool" {
+  name       = "default-pool"
+  location   = "us-west1-a"
+  cluster    = google_container_cluster.gke_cluster.name
+
+  initial_node_count = 3
+
+  autoscaling {
+    min_node_count = 3
+    max_node_count = 6
+  }
+
+  node_config {
+    machine_type = "e2-medium"
+    service_account = var.gke_sa_email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    tags = ["gke-node"]
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
