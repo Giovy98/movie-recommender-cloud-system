c17a59c51d7401cde5774dfb4daa3dff069269c4
Giovanni <giovy1232@gmail.com>
2025-05-03 14:50:22 +0200

    Fix SA

--- terraform-gcp-sa-managed/service-account.tf
+++ terraform-gcp-sa-managed/service-account.tf
@@ -1,9 +1,3 @@
-resource "google_project_service" "api" {
-  for_each           = toset(local.apis)
-  service            = each.key
-  disable_on_destroy = false
-}
-
 resource "google_project_iam_member" "assign_role" {
   for_each = toset(local.roles)
   project  = local.project_id
