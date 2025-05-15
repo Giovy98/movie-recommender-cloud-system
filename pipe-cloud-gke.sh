f6cd3ce6270daf0fd553574862b8d1421a413992
Giovanni <giovy1232@gmail.com>
2025-05-15 20:24:53 +0200

    Modify Main with new Feature

--- pipe-cloud-gke.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Submit del workflow Argo
-echo "Submitting Argo workflow..."
-argo submit k8s-gke/argoWorkflow/job-test-cloud.yaml -n argo --watch
-
-# Deploy dei servizi API e UI
-echo "Applying Kubernetes deployments and services..."
-kubectl apply -f k8s-gke/deployment/
