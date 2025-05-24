b9b759fd95a19644b259efa287ae97bf79cdfa6b
Giovanni <giovy1232@gmail.com>
2025-05-24 15:26:47 +0200

    Update: TEST

--- ui_service/app.py
+++ ui_service/app.py
@@ -41,7 +41,7 @@ def get_recommendations(movie_title: str):
 # === Interfaccia Streamlit ===
 def main():
     st.set_page_config(page_title="Movie Recommender", layout="centered")
-    st.title("🎬 Movie Recommandation Films !")
+    st.title("🎬 Movie Recommandation Films ")
 
     movie_list = load_movies_from_gcs()
 
